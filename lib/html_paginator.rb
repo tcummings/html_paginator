3c55aae5be135a00702b97c08d6183785d487e12
tcummings <youremail@example.com>
2011-07-28 10:01:05 -0700

    added addition exception handling, updated tests

--- lib/html_paginator.rb
+++ lib/html_paginator.rb
@@ -1,5 +1,5 @@
 require 'nokogiri'
-
+class InvalidPageNumber < RangeError; end
 # Summary:html_paginator will paginate html based on the page_size.
 #         All html tags will be maintained from the original html structure
 #
@@ -8,8 +8,10 @@ class HTML_paginator
 
   public
   def initialize(input,page_size)
+    raise ArgumentError, "Input cannot be empty" if input.empty?
+    raise ArgumentError, "Page size must be greater then zero" unless page_size > 0
     @html = Nokogiri::HTML(input)
-    @page_size = page_size > 0 ? page_size : 1 #page_size must be greater then 0
+    @page_size = page_size
     @total_words_count = @html.text().split(/\s+/).size
     # @total_pages should account for partial pages
     @total_pages = (@html.text().split(/\s+/).size/(@page_size*1.0)).ceil
@@ -17,10 +19,9 @@ class HTML_paginator
 
   def get_page(page_number)
     # only allow valid page numbers 1 - total pages
-    if page_number < 1 then 
-      page_number = 1
-    elsif page_number > @total_pages then 
-      page_number = @total_pages
+    if (page_number < 1 || page_number > @total_pages) then 
+      start_page = @total_pages < 1 ? @total_pages : 1 
+      raise InvalidPageNumber, "Invalid page,valid pages are #{start_page} through #{@total_pages}."
     end
     #make a copy of the source so we do not modify the original
     doc = Nokogiri::HTML(@html.to_s)
@@ -41,7 +42,7 @@ class HTML_paginator
     page_end = @page_size * page_number 
     page_begin = (page_end - @page_size)+1
     
-    # a work index to track our current place in the text
+    # a word index to track our current place in the text
     word_index = 0
     # gurantee the end index does not pass the end of the text
     if page_end > @total_words_count then page_end = @total_words_count end
--- spec/lib/html_paginator_spec.rb
+++ spec/lib/html_paginator_spec.rb
@@ -16,12 +16,27 @@ describe HTML_paginator do
     @HTML_paginator = HTML_paginator.new(input, 4)
   end
 
+  describe "initialize" do
+   
+    it "should raise an ArgumentError exception if the page size is less then 1" do
+      some_HTML = <<-html
+      <html>
+        <div>
+          <p>some HTML</p>
+        </div>
+      <html>
+      html
+      lambda {HTML_paginator.new(some_HTML, 0)}.should raise_error(ArgumentError)
+    end
+
+    it "should raise an ArgumentError exception if the input is empty" do
+      lambda {HTML_paginator.new("",1)}.should raise_error(ArgumentError)
+    end
+
+  end
+  
   describe "get_page" do
 
-    it "should response to get_page" do
-      @HTML_paginator.should respond_to(:get_page)
-    end
-    
     it "should return properly formed html with tags maintained for page 1" do
       page = @HTML_paginator.get_page(1)
       page.inner_html.should eq("<div><p>this is a<a>long</a></p></div>")
@@ -37,11 +52,10 @@ describe HTML_paginator do
       page.inner_html.should eql("<div><p>paginated</p></div>")
     end
 
-    it "should nil if input is empty" do
-      empty_html_paginator = HTML_paginator.new("", 4)
-      page = empty_html_paginator.get_page(1)
-      page.should be_nil
+    it "should raise an exception if an invalid page number is entered" do
+      lambda {@HTML_paginator.get_page(0)}.should raise_error(InvalidPageNumber)
     end
+
   end
 
   describe "print_page" do
